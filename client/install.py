66b0bc6c25ec1405379566fcb5e5c9f5073d93a1
Chris Jackson <hello@cjjackson.dev>
2024-12-25 16:27:44 +0000

    feat: added install script for `client` and `local-incus`

---
+++ client/install.py
@@ -0,0 +1,14 @@
+#!/usr/bin/evn python3
+import getpass
+import os
+import shutil
+import sys
+
+if getpass.getuser() != "root":
+    print("Must be root!", file=sys.stderr)
+    exit(1)
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+shutil.copy("run-deploy-remote-cli.py", "/usr/local/bin/run-deploy-remote-cli")
+shutil.copy("run-deploy-remote-metal-cli.py", "/usr/local/bin/run-deploy-remote-metal-cli")
---
+++ local-incus/install.py
@@ -0,0 +1,16 @@
+#!/usr/bin/evn python3
+import getpass
+import os
+import shutil
+import sys
+
+if getpass.getuser() != "root":
+    print("Must be root!", file=sys.stderr)
+    exit(1)
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.makedirs("/opt/run-deploy/bin", exist_ok=True)
+os.mkdir("/opt/run-deploy/image")
+
+shutil.copy("run-deploy.py", "/opt/run-deploy/bin/run-deploy")
+shutil.copy("run-deploy-cli.py", "/opt/run-deploy/bin/run-deploy-cli")
