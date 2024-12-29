8d97cbf31312f05642c98dd069ad9e8a76d07551
Chris Jackson <hello@cjjackson.dev>
2024-12-29 09:20:27 +0000

    feat: use dataclass to deal with toml for `run-deploy-image-toml.py`

--- toml-util/run-deploy-image-toml.py
+++ toml-util/run-deploy-image-toml.py
@@ -10,6 +10,8 @@ import subprocess
 import sys
 import time
 import tomllib
+from dataclasses import dataclass
+from typing import Self
 
 
 def error_and_exit(error_name: str, message: str):
@@ -48,75 +50,140 @@ except (OSError, tomllib.TOMLDecodeError):
         "Unable to open toml manifest"
     )
 
-image_name = ""
-try:
-    image_name = toml_manifest["name"]
-except KeyError:
-    error_and_exit(
-        "NO_NAME",
-        "Must have a name"
-    )
-file_name_validation(image_name, "name", True)
 
-build_script = ""
-try:
-    build_script = os.path.abspath(toml_manifest["build_script"])
-except KeyError:
-    error_and_exit(
-        "BUILD_SCRIPT_MISSING",
-        "`build_script` is required"
-    )
+class ManifestDataError(Exception):
+    pass
 
-manifest_json = toml_manifest.get("manifest", {})
-if not manifest_json:
-    error_and_exit(
-        "JSON_MANIFEST",
-        "Must have a manifest"
-    )
 
+class BuildDataError(Exception):
+    pass
+
+
+@dataclass()
+class ManifestData:
+    incus_name: str = ""
+    image_dir: str = ""
+    exec: str = ""
+    stamp: float = 0
+
+    @classmethod
+    def create(cls, data: dict) -> Self:
+        incus_name = incus_name=data.get("incus-name", "")
+        if incus_name:
+            file_name_validation(incus_name, "incus-name", True)
+        return cls(incus_name)
+
+    def make_json_dict(self) -> dict:
+        if self.incus_name:
+            return {
+                "incus-name": self.incus_name,
+                "image-dir": self.image_dir,
+                "exec": self.exec,
+                "stamp": self.stamp
+            }
+        else:
+            return {
+                "image-dir": self.image_dir,
+                "exec": self.exec,
+                "stamp": self.stamp
+            }
+
+
+@dataclass(frozen=True)
+class BuildData:
+    name: str
+    build_script: str
+    manifest: dict[str, ManifestData]
+    tmp_locaiton: str = "/tmp"
+
+    @classmethod
+    def create(cls, data: dict) -> Self:
+        if "manifest" not in data:
+            raise ManifestDataError("Must have at least one manifest")
+        manifest = data["manifest"]
+        for key, value in manifest.items():
+            manifest[key] = ManifestData.create(value)
+
+        if flag_hostname:
+            if '__' not in manifest:
+                raise ManifestDataError("Manifest is not setup for `--hostname`")
+            manifest = {flag_hostname: manifest['__']}
+
+        if "name" not in data:
+            raise BuildDataError("Must have 'name'")
+        name = data["name"]
+        file_name_validation(name, "name", True)
+
+        if "build_script" not in data:
+            raise BuildDataError("Must have 'build_script'")
+        build_script = os.path.abspath(data["build_script"])
+
+        return cls(
+            name=name,
+            build_script=build_script,
+            manifest=manifest,
+            tmp_locaiton=data.get("tmp_location", "/tmp")
+        )
+
+
+    def make_manifest_json_dict(self) -> dict:
+        data = {}
+        for key, value in self.manifest.items():
+            data[key] = value.make_json_dict()
+        return data
+
+
+    def update_manifest(self, script_name: str, stamp: float):
+        for _, value in self.manifest.items():
+            value.image_dir = self.name
+            value.exec = script_name
+            value.stamp = stamp
+
+build_data = None
 try:
-    if flag_hostname:
-        manifest_json = {flag_hostname: manifest_json["__"]}
-except KeyError:
+    build_data = BuildData.create(toml_manifest)
+except ManifestDataError as e:
     error_and_exit(
-        "HOSTNAME_FLAG",
-        "There isn't a flag set for either `flag_hostname`"
+        "MANIFEST_DATA_ERROR",
+        e.__str__()
     )
+except BuildDataError as e:
+    error_and_exit(
+        "BUILD_DATA_ERROR",
+        e.__str__()
+    )
+if not build_data:
+    exit(0)
 
 project_path = os.path.dirname(os.path.abspath(arg_toml))
 os.chdir(project_path)
 
-tmp_dir = f"{toml_manifest.get('tmp_location', '/tmp')}/run-deploy-image-{image_name}-{time.time()}"
+tmp_dir = f"{build_data.tmp_locaiton}/run-deploy-image-{build_data.name}-{time.time()}"
 os.mkdir(tmp_dir, 0o700)
 os.chdir(tmp_dir)
 os.makedirs("mnt/_deploy")
 
 now = datetime.datetime.now(datetime.UTC)
-script_name = f"{image_name}-{now.year}-{now.month:02d}-{now.day:02d}_{now.hour:02d}-{now.minute:02d}-{now.second:02d}"
+script_name = f"{build_data.name}-{now.year}-{now.month:02d}-{now.day:02d}_{now.hour:02d}-{now.minute:02d}-{now.second:02d}"
 squashfs_name = f"{script_name}.squashfs"
 
 script_path = pathlib.Path(f"mnt/_deploy/{script_name}")
 script_path.write_text(f"""#!/bin/dash
-cd /opt/run-deploy/image/{image_name}
-ln -sf {squashfs_name} {image_name}.squashfs || exit 1
-/opt/local/script/deploy/{image_name} || echo "/opt/run-deploy/script/deploy/{image_name} not found or incorrect permission!" && exit 0
+cd /opt/run-deploy/image/{build_data.name}
+ln -sf {squashfs_name} {build_data.name}.squashfs || exit 1
+/opt/local/script/deploy/{build_data.name} || echo "/opt/run-deploy/script/deploy/{build_data.name} not found or incorrect permission!" && exit 0
 """, 'utf-8')
 script_path.chmod(0o755)
 
-for key, _ in manifest_json.items():
-    manifest_json[key]["image-dir"] = image_name
-    manifest_json[key]["exec"] = script_name
-    manifest_json[key]["stamp"] = now.timestamp()
-    if "incus-name" in manifest_json[key]:
-        file_name_validation(manifest_json[key]["incus-name"], "incus-name", True)
+build_data.update_manifest(script_name, now.timestamp())
 
 with open("mnt/_deploy/push.json", "w") as f:
-    json.dump(manifest_json, f)
+    json.dump(build_data.make_manifest_json_dict(), f)
 
 # Run build script
 try:
     subprocess.run([
-        build_script
+        build_data.build_script
     ], check=True, env={"RUN_DEPLOY_PROJECT_PATH": project_path} | os.environ)
 except subprocess.CalledProcessError:
     error_and_exit(
