81f058a63781e6fcd58b3100d60a67e46d96c2c6
Chris Jackson <hello@cjjackson.dev>
2024-12-31 08:15:58 +0000

    feat: added uv mode for toml-util

--- toml-util/install.py
+++ toml-util/install.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
+import argparse
 import getpass
 import os
+import pathlib
 import shutil
 import sys
+
+parser = argparse.ArgumentParser(description="Install toml util")
+parser.add_argument("--uv", action='store_true', help="Place uv shebang on top of file")
+args = parser.parse_args()
+
+flag_uv = args.uv
 
 if getpass.getuser() != "root":
     print("Must be root!", file=sys.stderr)
@@ -10,6 +18,18 @@ if getpass.getuser() != "root":
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-shutil.copy("run-deploy-image-toml.py", "/usr/local/bin/run-deploy-image-toml")
-shutil.copy("run-deploy-local-toml.py", "/usr/local/bin/run-deploy-local-toml")
-shutil.copy("run-deploy-remote-toml.py", "/usr/local/bin/run-deploy-remote-toml")
+uv_stub = None
+if flag_uv:
+    uv_stub = pathlib.Path("../uv_stub.py").read_text("utf-8").strip() + "\n"
+
+def copy(src: str, dest: str):
+    if uv_stub:
+        src_str = pathlib.Path(src).read_text("utf-8").removeprefix("#!/usr/bin/env python3").strip()
+        dest_str = f'{uv_stub}{src_str}' + "\n"
+        pathlib.Path(dest).write_text(dest_str, 'utf-8')
+    else:
+        shutil.copy(src, dest)
+
+copy("run-deploy-image-toml.py", "/usr/local/bin/run-deploy-image-toml")
+copy("run-deploy-local-toml.py", "/usr/local/bin/run-deploy-local-toml")
+copy("run-deploy-remote-toml.py", "/usr/local/bin/run-deploy-remote-toml")
