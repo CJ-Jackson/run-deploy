764eddd81ecfcc6a9f6295013e0f3073291b5dbf
Chris Jackson <hello@cjjackson.dev>
2025-01-24 08:51:32 +0000

    chore: fifo is the way to go with output

--- remote-metal/run-deploy-socket.py
+++ remote-metal/run-deploy-socket.py
@@ -3,44 +3,60 @@ import json
 import os
 import socket
 import sys
+import time
 
 arg_cmd = sys.argv[1]
 commands: dict = {}
 
+
+def handle_fifo(fifo_path: str):
+    with open(fifo_path, "r") as fifo:
+        data = json.load(fifo)
+        if data["code"] > 0:
+            print(data["stderr"], file=sys.stderr)
+            exit(data["code"])
+        else:
+            print(data["stdout"])
+    os.remove(fifo_path)
+
+
 def send_cli():
+    fifo_path = f"/tmp/run-deploy-cli-fifo-{time.time()}"
+    os.mkfifo(fifo_path, 0o666)
+
     data = {
         "cmd": "cli",
         "token": os.environ['RUN_DEPLOY_TOKEN'].strip(),
         "key": os.environ['RUN_DEPLOY_KEY'].strip(),
-        "args": sys.argv[2:]
+        "args": sys.argv[2:],
+        "fifo": fifo_path
     }
 
-    rtn_data = {}
     with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
         client.connect("/tmp/run-deploy.sock")
         client.sendall(bytes(json.dumps(data) + "\n", 'utf-8'))
-        client.accept()
 
-        rtn_data = client.recv(1024)
-    print(rtn_data)
+    handle_fifo(fifo_path)
 
 commands["cli"] = send_cli
 
 
 def deploy():
+    fifo_path = f"/tmp/run-deploy-fifo-{time.time()}"
+    os.mkfifo(fifo_path, 0o666)
+
     data = {
         "cmd": "deploy",
         "target": sys.argv[2].strip(),
         "key": sys.argv[3].strip(),
+        "fifo": fifo_path
     }
 
-    rtn_data = {}
     with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
         client.connect("/tmp/run-deploy.sock")
         client.sendall(bytes(json.dumps(data) + "\n", 'utf-8'))
 
-        rtn_data = client.recv(1024)
-    print(rtn_data)
+    handle_fifo(fifo_path)
 
 
 commands["deploy"] = deploy
@@ -48,9 +64,9 @@ commands["deploy"] = deploy
 
 def recv():
     data = json.load(sys.stdin)
-    # json.dump(data, sys.stdout)
-    sys.stdout.write(json.dumps(data) + 'rn')
-    exit(0)
+    fifo_path = data["fifo"]
+    with open(fifo_path, "w") as fifo:
+        json.dump(data, fifo)
 
 
 commands["recv"] = recv
